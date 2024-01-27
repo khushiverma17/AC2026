6c14a3f7ad987c7af6bf277989cf465edb030c47
khushi <khushiverma1708@gmail.com>
2024-01-27 13:28:54 +0530

    added

---
+++ Day24/Task2.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int totalFruit(vector<int>& fruits) {
+        int i=0;
+        int ans=0;
+        unordered_map<int,int>basket;
+        for(int j=0;j<fruits.size();j++)
+        {
+            basket[fruits[j]]++;
+            while(basket.size()>2)
+            {
+                basket[fruits[i]]--;
+                if(basket[fruits[i]]==0)
+                {
+                    basket.erase(fruits[i]);
+                }
+                i++;
+            }
+            ans=max(ans,j-i+1);
+        }
+        return ans;
+
+
+
+    }
+
+
+int main(){
+    vector<int>fruits{1,2,3,2,2};
+    cout<<totalFruit(fruits);
+    return 0;
+}
